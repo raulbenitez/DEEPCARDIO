62298f5c6ba4d51a64b921a9a898a795d704ef21
aleixsacrest <aleixsacrest>
2021-01-09 19:52:42 +0100

    inception.py - gaussian filter option when loading data - currying recall_and_false_spark metric function, allows to choose the ratio - sigmoid_loss

--- sparks/readme.md
+++ sparks/readme.md
@@ -1,7 +1,15 @@
-_out
-deepcardio_utils.py - functions and utils (used in the other files)
-exploreImageAndPlotSpark.ipynb - jupyter where different strategies are explored
+_out - Output images and videos.
+deepcardio_utils.py - Functions and utils (used in the other files).  
+explore.ipynb - Jupyter where different strategies are explored.  
 
-### Generate different types of movies from frames in ../_datasets/deepcardio/170215_RyR-GFP30_RO_01_Serie2_SPARKS-calcium
-generateMovie.py - regular movie
-generateMovie-locateSpark.py - drawing a circumference over the sparks
+fullSparkPixels.czsv - It is the concatenation of all pixels in all frames of 170215_RyR-GFP30_RO_01_Serie2_SPARKS-calcium movie. The purpose was to build a unique gmm with all pixels. Not very useful at the end.  
+
+### train/  
+inception.py - Build and train inceptionv3 neural networks.
+inception_test.ipynb - Display different metrics for each trained model (confusion matrix, recalls...).
+imagenet_inception.ipynb - Transfer learning with imagenet.
+
+### Generate movies from frames in ../_datasets/deepcardio/170215_RyR-GFP30_RO_01_Serie2_SPARKS-calcium  
+makeMovie.py - Regular movie.  
+makeMovieCircleSpark.py - Drawing a circumference over the sparks.  
+makeMovieGMMSpark.py - Circle spark area, and when it is visible it is highlighted in green using gmms over the filtered image. The video i the concatenation of the original image, the image with circle+highlight and the filtered image.
--- sparks/train/inception.py
+++ sparks/train/inception.py
@@ -1,20 +1,18 @@
 import os
-import pandas as pd
-
 import keras
-
-import tensorflow as tf
-
 import cv2
+import tensorflow as tf
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from skimage.filters import gaussian
 from keras.utils import np_utils
-
 from sklearn.model_selection import train_test_split
 
 from deepcardio_utils import IMAGE_FOLDER, get_frame_wise_classification
 
 
-def load_data(classesFromFile=False, imageFolder=IMAGE_FOLDER):
+def load_data(classesFromFile=False, imageFolder=IMAGE_FOLDER, gaussianFilter=True):
     # from images
     # imagePaths = sorted([img for img in os.listdir(imageFolder) if img.endswith(".tif")])
     # imageIdxs = list(range(len(imagePaths)))
@@ -22,9 +20,17 @@ def load_data(classesFromFile=False, imageFolder=IMAGE_FOLDER):
     # np.save(IMAGE_FOLDER+'/full_images.npy', images)
 
     images = np.load(os.path.join(imageFolder, 'full_images.npy'))
+
+    if gaussianFilter:
+        images = [gaussian(im, sigma=1, multichannel=True, preserve_range=True).astype('uint8') for im in images]
+
+    # plt.figure(figsize=(20, 10))
+    # plt.imshow(cv2.cvtColor(images[0], cv2.COLOR_BGR2RGB))
+    # plt.show()
+
     # reshape for cnn input
     minsize = 75
-    images = np.array([np.concatenate((im, np.full((minsize-im.shape[0], 256, 3), 0))) for im in images])
+    images = np.array([np.concatenate((im, np.full((minsize-im.shape[0], 256, 3), 0))).astype('uint8') for im in images])
 
     imageIdxs = list(range(images.shape[0]))
 
@@ -83,35 +89,39 @@ def metric_08recall_02accuracy(y_true, y_pred):
 def _sigmoid(x):
     return 2/(1+tf.math.exp(-5*x))-1
 
-def metric_09recall_01sigmoid_wrong_spark(y_true, y_pred):
-    # metrica ponderada 0.2 accuracy + 0.8 recall (classes spark exclusivament), sempre que hi hagi sparks, sino acc
-    realSparks = tf.math.argmax(y_true, axis=1) == 1
-    predSparks = tf.math.argmax(y_pred, axis=1) == 1
-    wellPredSparks = realSparks & predSparks
-    wellPredNoSparks = ~realSparks & ~predSparks
-    wrongPredSparks = ~realSparks & predSparks
+def _recall_and_false_spark(ratio=.6):
+    def recall_and_false_spark(y_true, y_pred):
+        # metrica ponderada 0.2 accuracy + 0.8 recall (classes spark exclusivament), sempre que hi hagi sparks, sino acc
+        realSparks = tf.math.argmax(y_true, axis=1) == 1
+        predSparks = tf.math.argmax(y_pred, axis=1) == 1
+        wellPredSparks = realSparks & predSparks
+        wellPredNoSparks = ~realSparks & ~predSparks
+        wrongPredSparks = ~realSparks & predSparks
 
-    totalElementsCount = tf.cast(tf.shape(y_true)[0], np.float32)
-    realSparksCount = tf.math.reduce_sum(tf.cast(realSparks, np.float32))
-    wellPredSparksCount = tf.math.reduce_sum(tf.cast(wellPredSparks, np.float32))
-    wellPredCount = tf.math.reduce_sum(tf.cast(wellPredNoSparks, np.float32)) + wellPredSparksCount
-    wrongPredSparksCount = tf.math.reduce_sum(tf.cast(wrongPredSparks, np.float32))
-    realNonSparksCount = tf.math.reduce_sum(tf.cast(~realSparks, np.float32))
+        totalElementsCount = tf.cast(tf.shape(y_true)[0], np.float32)
+        realSparksCount = tf.math.reduce_sum(tf.cast(realSparks, np.float32))
+        wellPredSparksCount = tf.math.reduce_sum(tf.cast(wellPredSparks, np.float32))
+        wellPredCount = tf.math.reduce_sum(tf.cast(wellPredNoSparks, np.float32)) + wellPredSparksCount
+        wrongPredSparksCount = tf.math.reduce_sum(tf.cast(wrongPredSparks, np.float32))
+        realNonSparksCount = tf.math.reduce_sum(tf.cast(~realSparks, np.float32))
 
-    recall = wellPredSparksCount / realSparksCount
-    accuracy = wellPredCount / totalElementsCount
-    wrongSparksMetricAux = wrongPredSparksCount / realNonSparksCount
-    wrongSparksMetric = 1 - _sigmoid(wrongSparksMetricAux)
+        recall = wellPredSparksCount / realSparksCount
+        accuracy = wellPredCount / totalElementsCount
+        wrongSparksMetricAux = wrongPredSparksCount / realNonSparksCount
+        wrongSparksMetric = 1 - _sigmoid(wrongSparksMetricAux)
 
 
-    # ret =  recall * 0.8 + accuracy * 0.2if realSparksCount > 0 else accuracy
-    return tf.cond(tf.greater(realSparksCount, 0),
-                   lambda: recall * 0.6 + wrongSparksMetric * 0.4,
-                   lambda: wrongSparksMetric)
+        # ret =  recall * 0.8 + accuracy * 0.2if realSparksCount > 0 else accuracy
+        return tf.cond(tf.greater(realSparksCount, 0),
+                       lambda: recall * ratio + wrongSparksMetric * (1-ratio),
+                       lambda: wrongSparksMetric)
+    return recall_and_false_spark
 
 def sigmoid_spark_and_non_spark_loss(y_true, y_pred):
-    realSparks = tf.math.argmax(y_true, axis=-1) == 1
-    predSparks = tf.math.argmax(y_pred, axis=-1) == 1
+    mul = tf.constant([0, 1], shape=[2, 1], dtype=y_true.dtype)
+
+    realSparks = tf.squeeze(tf.matmul(y_true, mul) == 1)
+    predSparks = tf.squeeze(tf.matmul(y_pred, mul) == 1)
     wrongPredSparks = ~realSparks & predSparks
     wrongPredNonSparks = realSparks & ~predSparks
 
@@ -130,6 +140,15 @@ def sigmoid_loss(y_true, y_pred):
     return _sigmoid(tf.reduce_mean(tf.square(y_true-y_pred), axis=-1))
 
 if __name__=='__main__':
+    a = tf.cast(tf.convert_to_tensor([[[0, 1] if np.random.randint(0,2) else [1, 0] for _ in range(5)] for _ in range(10)]), np.float32)
+    b = tf.cast(tf.convert_to_tensor([[[0, 1] if np.random.randint(0, 2) else [1, 0] for _ in range(5)] for _ in range(10)]), np.float32)
+    sigmoid_loss(a, b)
+    sigmoid_spark_and_non_spark_loss(a, b)
+
+    a = tf.convert_to_tensor([[0, 1] if np.random.randint(0, 2) else [1, 0] for _ in range(5)])
+    b = tf.convert_to_tensor([[0, 1] if np.random.randint(0, 2) else [1, 0] for _ in range(5)])
+    sigmoid_spark_and_non_spark_loss(a, b)
+
     X_train, Y_train, X_valid, Y_valid = load_data(classesFromFile=True)
 
     # Inception V3
@@ -140,7 +159,7 @@ if __name__=='__main__':
     epochs = 25
 
     opt = keras.optimizers.RMSprop(learning_rate=0.0001, decay=1e-6)
-    modelv3.compile(loss=sigmoid_spark_and_non_spark_loss, optimizer=opt, metrics=[metric_09recall_01sigmoid_wrong_spark])
+    modelv3.compile(loss=sigmoid_loss, optimizer=opt, metrics=[_recall_and_false_spark(0.6)])
 
     modelv3.fit(X_train, Y_train, batch_size=batch_size, epochs=epochs, validation_data=(X_valid, Y_valid), shuffle=True)
 
